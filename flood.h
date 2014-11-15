e5dcf8d416c8ad94ecc021edecf49a8fb3d986a8
Elivelton Costa <elivelton_costa@live.com>
2014-11-15 14:33:21 -0300

    Update flood.h
    Adicionado a função de manipulação de arquivo.

--- flood.h
+++ flood.h
@@ -3,9 +3,12 @@
 
 /* --- Tipos definidos --- */
 
-int aleatorio, contl, contc, l, c, jogada, atual, acao;
+int aleatorio, contl, contc, l, c, jogada, atual, acao, erro;
 int tabuleiro[14][14];
 char opcao, comando;
+FILE *arquivo;
+
+
 
 /* --- Sub-rotinas --- */
 
@@ -17,12 +20,33 @@ char opcao, comando;
 void gerar_tabela(int l, int c);
 
 /* *
- * Realiza a atribuição de valores
+ * Realiza a exibição de valores
  * @param utilização de l para linhas
  * @param utilização de c para colunas
  */
 void exibir_tabela(int l, int c);
+
+/* *
+ * Realiza a execução de um novo jogo
+ * @param utilização de l para linhas
+ * @param utilização de c para colunas
+ */
 void novo_jogo();
+
+/* *
+ * Inundação de valores em toda tabela
+ * @param utilização de l para linhas
+ * @param utilização de c para colunas
+ */
 void inundar();
+
+void ler_matriz(int l, int c);
+
+/* *
+ * Salvar a matriz de um jogo
+ * @param utilização de l para linhas
+ * @param utilização de c para colunas
+ */
+void escrever_matriz(int l, int c);
 
 #endif
