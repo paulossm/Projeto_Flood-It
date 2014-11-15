04176e82783df0d80b54cc63ca326316aa9d8cbb
Elivelton Costa <elivelton_costa@live.com>
2014-11-15 14:36:04 -0300

    Update flood.c
    Adicionado a função de escrever matriz em um arquivo de texto(txt). Contudo, a função de ler ainda está sendo estudada.

--- flood.c
+++ flood.c
@@ -4,11 +4,11 @@
 #include <time.h>
 
 void gerar_tabela(int l, int c){
-	
+
 	srand( (unsigned)time(NULL) );
 	for(contl = 0; contl < l; contl++){
 		for(contc = 0; contc < c; contc++){
-			aleatorio = (rand() % 6); 
+			aleatorio = (rand() % 6);
 			tabuleiro[contl][contc] = aleatorio;
 		}
 	}
@@ -26,13 +26,70 @@ void exibir_tabela(int l, int c){
 void inundar(int L, int C, int tabuleiro[L][C], int l, int c, int atual, int comando ){
 	if(tabuleiro[l][c] == atual) {
 		tabuleiro[l][c] = comando;
-		if(l < 14){ inundar(14, 14, tabuleiro, l + 1, c, atual, comando); } 
+		if(l < 14){ inundar(14, 14, tabuleiro, l + 1, c, atual, comando); }
 		if(l > 0) { inundar(14, 14, tabuleiro, l - 1, c, atual, comando); }
 		if(c < 14){ (inundar(14, 14, tabuleiro, l, c + 1, atual, comando)); }
 		if(c > 0){ inundar(14, 14, tabuleiro, l, c - 1, atual, comando);}
 	}
 }
 
+void novo_jogo()
+{
+	printf("\n\nAqui está o tabuleiro. seu objetivo é preenchê-lo com um único valor começando pelo canto superior esquerdo. você tem 25 tentativas. Boa Sorte!\n");
+	gerar_tabela( 14, 14 );
+	exibir_tabela ( 14, 14);
+	printf("\n");
+}
+
+void ler_matriz(int l, int c){
+    arquivo = fopen("arquivo.txt","r+");
+
+    if(arquivo)
+    {
+        erro = 0;
+        for(contl = 0; contl < l; contl++)
+        {
+            for(contc = 0; contc < c; contc++)
+            {
+            // (!fscanf(arquivo,"%d", &tabuleiro[contl][contc]));
+            //  {
+            //      erro = 1;
+            //      printf("Erro a o ler a entrada (%d,%d) da matriz\n",contl,contc);
+            //        break;
+            }
+        }
+
+            if(erro)
+            break;
+        }
+        fclose(arquivo);
+    }
+    else
+    {
+    printf("Erro ao abrir o arquivo texto para leitura");
+    }
+}
+
+void escrever_matriz(int l, int c){
+    arquivo = fopen("arquivo.txt","w+");
+      if(arquivo)
+      {
+        for(contl = 0; contl < l; contl++)
+        {
+            for(contc = 0; contc < c; contc++)
+            {
+                fprintf(arquivo,"%d",tabuleiro[contl][contc]);
+            }
+            fprintf(arquivo,"\n");
+        }
+        fclose(arquivo);
+        printf("Jogo SALVO!!");
+      }
+      else
+      {
+        printf("Erro ao abrir o arquivo texto para escrita");
+      }
+}
 
 /* void menu_opcoes()
 {
@@ -50,7 +107,6 @@ void executa(int comando)
 		case 48: // comando '0'
 		    inundar(14, 14, tabuleiro, 0, 0, tabuleiro[0][0], 0);
 		    break;
-		
 		case 49: // comando '1'
 			inundar(14, 14, tabuleiro, 0, 0, tabuleiro[0][0], 1);
 			break;
@@ -70,17 +126,9 @@ void executa(int comando)
 			printf("\nO jogo está sendo encerrado...\n");
 			exit(1);
 		case 115: // comando 's'
-			printf("o s funciona!\n\n\n");
+			escrever_matriz(14,14);
+			exit(1);
 			break;
 		default:;
 	}
-}
-
-
-void novo_jogo()
-{
-	printf("\n\nAqui está o tabuleiro. seu objetivo é preenchê-lo com um único valor começando pelo canto superior esquerdo. você tem 25 tentativas. Boa Sorte!\n");
-	gerar_tabela( 14, 14 );
-	exibir_tabela ( 14, 14);
-	printf("\n");
 }
