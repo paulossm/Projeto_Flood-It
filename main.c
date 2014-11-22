a66667a152850b34b8e8153bc8ad61480956bf47
Elivelton Costa <elivelton_costa@live.com>
2014-11-22 18:25:57 -0300

    Update main.c
    Melhorias

--- main.c
+++ main.c
@@ -10,48 +10,52 @@
 
 
 int main( void ){
-	printf("**********************\n");
-	printf("BEM-VINDO AO FLOOD IT!\n");
-	printf("**********************\n");
-	printf("\n(?) Selecione uma entre as opções a seguir:\n");
-	printf("'n' Novo Jogo - ");
-	printf("'o' Carregar um jogo salvo\n");
+	//printf("**********************\n");
+	//printf("BEM-VINDO AO FLOOD IT!\n");
+	//printf("**********************\n");
+	printf("******************************\n\n");
+	printf("*** *   **** **** ***    * ***\n");
+	printf("*   *   *  * *  * *  *   *  *\n");
+	printf("**  *   *  * *  * *  *   *  *\n");
+	printf("*   *** **** **** ***    *  *");
+	printf("\n\n******************************");
+	printf("\n\n(?) Selecione uma entre as opções a seguir:\n");
+	printf("'n' Novo Jogo - 'o' Carregar um jogo salvo\n");
 
 	do
 	{
-		printf("\nSeu comando: "); scanf(" %c", &opcao);
-		acao = (int)opcao;
-	} while (acao < 110 || acao > 111);
-		
-		if(acao == 110)
-		{
-			novo_jogo();
-			partida:
-			for(;jogada <= 25; jogada++)
-			{
-				printf("\n(!) Tentativa: %i/25\n", jogada);
-				printf("------------------\n");
-				printf("(?) Opções: 's' salvar o jogo atual - 'q' encerrar o jogo\n");
-				printf("seu comando: ");
-				scanf(" %c", &comando);
-				executa((int)comando);
-					if(erro==1){
-						jogada--;
-						erro = 0;
-					}
-				if(cond==1){
-					printf("\n\n(!)Você venceu.!\n");
-					exit(1);
-				}
-			}
-			printf("\n\n(!)Você perdeu.\n");
-		}
-		else {
-			if(acao==111) {
-			/* CARREGAR UM JOGO ANTERIOR */
-			executa(acao);
-			goto partida;
-			}
-		}
+		printf("\nSeu comando: ");
+		scanf(" %c", &opcao);
+	} while ((int)opcao < 110 || (int)opcao > 111);
+
+    if((int)opcao == 110)
+    {
+        novo_jogo();
+        partida:
+        for(;jogada <= 25; jogada++)
+        {
+            printf("\n(!) Tentativa: %i/25\n", jogada);
+            printf("------------------\n");
+            printf("(?) Opções: 's' salvar o jogo atual - 'q' encerrar o jogo\n");
+            printf("seu comando: ");
+            scanf(" %c", &comando);
+            executa((int)comando);
+            if(erro == 1){
+                jogada--;
+                erro = 0;
+            }
+            if(cond == 1){
+                printf("\n\n(!)Você venceu.!\n");
+                exit(1);
+            }
+        }
+        printf("\n\n(!)Você perdeu.\n");
+    }
+
+    else{
+        /* CARREGAR UM JOGO ANTERIOR */
+        executa((int)opcao);
+        goto partida;
+    }
 	return 0;
 }
