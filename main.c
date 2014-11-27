216f783175ce08df9bff3af0bb50b5aca2498192
Elivelton Costa <elivelton_costa@live.com>
2014-11-26 23:36:33 -0300

    Update main.c
    Documentado

--- main.c
+++ main.c
@@ -1,13 +1,13 @@
 /* ********************************************************************* *\
 |                                                                         |
-|       PROJETO DO SIMULADOR DO GAME "FLOOD IT" EM LINGUAGEM "C".		  | 
-|		 BACHARELADO EM TECNOLOGIA DA INFORMAÇÃO - UFRN - 2014.			  |
-|			INTRODUÇÃO/PRÁTICAS DE TÉCNICAS DE PROGRAMAÇÃO				  |
+|       PROJETO DO SIMULADOR DO GAME "FLOOD IT" EM LINGUAGEM "C".	  |
+|		 BACHARELADO EM TECNOLOGIA DA INFORMAÇÃO - UFRN - 2014.	  |
+|			INTRODUÇÃO/PRÁTICAS DE TÉCNICAS DE PROGRAMAÇÃO	  |
 |                                                                         |
 \* ********************************************************************* */
 
 /* ------------------------------------------------------------------------
-								Autores			
+								Autores
                         ______________________
 
                         Elivelton Costa Silva
@@ -27,41 +27,52 @@ int main( void ){
 	printf("\n\n\t******************************");
 	printf("\n\n\t'n' > Novo Jogo\n\t'o' > Carregar Jogo\n");
 
+ /*Estrutura responsável para receber o valor correto para inicar jogo ou carregar uma anterior
+  *Só finalizará se receber um char com valor 'n' ou 'o'
+  */
 	do
 	{
 		printf("\n\tOpção >> ");
 		scanf(" %c", &opcao);
 	} while (opcao - 48 < 62 || opcao - 48 > 63);
 
+    /*Estrura utilizada para inicialização do jogo
+     *Caso a opção selecionada seja 'n'
+     */
     if((int)opcao == 110)
     {
-        novo_jogo();
+        novo_jogo(); //Criação jogo. Definindo dificuldades
         partida:
         for(;jogada <= padrao.max_jogadas; jogada++)
         {
             printf("\n\t\t\tTentativa: %i/%i\n\n\t's' > salvar o jogo atual\n\t'q' > encerrar o jogo\n", jogada, padrao.max_jogadas);
             printf("\tOpção >> ");
-            scanf(" %c", &comando);
-            
-            executar((int)comando);
-				
+            scanf(" %c", &comando); //Atribuição de valores em cada jogada
+
+            executar((int)comando); //Execução do jogo em cada jogada
+
+				/*Verificação de valor do caractere digitado*/
 				if(entrada_invalida){
 					jogada--;
 					entrada_invalida = Falso;
 				}
-				
+
+                /*verificação de vitoria em cada jogada do jogo */
 				if(vitoria){
 					printf("\n\n\t(i) PARABÉNS! Você venceu!\n");
-					exit(1);
+					exit(1); //Saída do jogo
 				}
         }
         printf("\n\n\t(i) Você perdeu!\n");
     }
 
+    /*Caso a opção selecionada seja 'o'
+     *Irá para um dos casos da função executar
+     */
     else{
         // Carregar um Jogo Anterior
-        executar((int)opcao);
-        goto partida;
+        executar((int)opcao);   //Função responsável pela execução do jogo
+        goto partida;   //retorno para partida que se enconta no if do main.c, sem a necessidade de chamar novojogo()
     }
 	return 0;
 }
